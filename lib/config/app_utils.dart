bc02e4ef8cc191697d980e9972d0c320c5a2a148
Efen Stakes <efenstakes101@gmail.com>
2020-04-14 20:04:00 +0300

    add app colors

--- lib/config/app_utils.dart
+++ lib/config/app_utils.dart
@@ -3,18 +3,23 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   
-  static final Color primary = Color(0x3F92B7);
-  static final Color secondary = Color(0x6CC4DC);
-  static final Color tertiary = Color(0xAACDE5);
-  static final Color tertiaryAlt = Color(0xE3EFF9);
+  static final Color primary = Color.fromRGBO(63, 146, 183, 1);
+  static final Color secondary = Color.fromRGBO(108, 196, 220, 1); 
+  static final Color tertiary = Color.fromRGBO(170, 205, 229, 1);
+  static final Color tertiaryAlt = Color.fromRGBO(227, 239, 249, 1);
 
-  static final Color sucsess = Color(0x67A53D);
-  static final Color error = Color(0xE57373);
-  static final Color warning = Color(0xFFD54F);
+  static final Color sucsess = Color.fromRGBO(103, 165, 61, 1);
+  static final Color success = Color.fromRGBO(103, 165, 61, 1);
+  static final Color error = Color.fromRGBO(229, 115, 115, 1);
+  static final Color warning = Color.fromRGBO(255, 213, 79, 1)
+                                    .withOpacity(0.8);
 
-  static final Color white = Color(0xFFFFFF);
-  static final Color black = Color(0x000000);
-  static final Color blackAlt = Color(0x000000)
-                                  .withOpacity(0.4);
+  static final Color white = Color.fromRGBO(255, 255, 255, 1)
+                                  .withOpacity(0.9);
+
+  static final Color black = Color.fromRGBO(0, 0, 0, 1).withOpacity(0.5);
+  static final Color blackAlt1 = Color.fromRGBO(0, 0, 0, 1).withOpacity(0.5);
+  static final Color blackAlt2 = Color.fromRGBO(0, 0, 0, 1)
+                                     .withOpacity(0.4);
 
 }
