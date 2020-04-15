3934cf89d422c28a692e75a65bb883baf9803c71
Efen Stakes <efenstakes101@gmail.com>
2020-04-16 00:05:23 +0300

    add app assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kiloko/config/app_routes.dart';
+import 'package:kiloko/config/app_utils.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,57 +9,110 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Kiloko',
+
       theme: ThemeData(
         
-        primarySwatch: Colors.blue,
-        primaryColor: Color(0x3F92B7),
-        buttonColor: Color(0x3F92B7),
-        errorColor: Color(0xE57373),
+        // primarySwatch: AppColors.primary,
+        primaryColor: AppColors.primary,
+        buttonColor: AppColors.primary,
+        errorColor: AppColors.error,
+        
+
+        buttonTheme: ButtonThemeData(
+          buttonColor: AppColors.primary,
+          splashColor: AppColors.tertiary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
+        ),
+
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: AppColors.primary,
+          elevation: 6,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
+        ),
+
+        primaryIconTheme: IconThemeData(
+          size: 28,
+          color: AppColors.white
+        ),
+
+        accentIconTheme: IconThemeData(
+          size: 32,
+          color: AppColors.white
+        ),
+
+        appBarTheme: AppBarTheme(
+          color: AppColors.secondary,
+          elevation: 10,
+          textTheme: TextTheme(
+            title: Theme.of(context).textTheme.title
+          ),
+          iconTheme: IconThemeData(
+            size: 28,
+            color: Colors.black
+          ),
+          actionsIconTheme: IconThemeData(
+            size: 28,
+            color: Colors.black
+          ),
+        ),
 
         textTheme: TextTheme(
           
           title: TextStyle(
             fontSize: 28,
-            fontWeight: FontWeight.w700,
-            color: Color(0x000000),
+            fontWeight: FontWeight.w600,
+            // color: Color(0xFFFF9000),
+            color: Colors.black,
           ),
           subtitle: TextStyle(
-            fontSize: 24,
+            fontSize: 26,
             fontWeight: FontWeight.w600,
-            color: Color(0x000000).withOpacity(0.8),
+            // color: Color(0x0000).withOpacity(0.8),
+            color: Colors.black,
           ),
           subhead: TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w600,
-            color: Color(0x000000).withOpacity(0.8),
-            fontStyle: FontStyle.italic,
+            // color: Color(0x000000).withOpacity(0.8),
+            color: Colors.black,
           ),
 
           body1: TextStyle(
-            fontSize: 24,
+            fontSize: 20,
             fontWeight: FontWeight.normal,
-            color: Color(0x000000),
+            // color: Color(0x000000),
+            color: Colors.black,
           ),
           body2: TextStyle(
             fontSize: 22,
             fontWeight: FontWeight.normal,
-            color: Color(0x000000),
+            // color: Color(0x000000),
+            color: Colors.black,
           ),
 
           display1: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.normal,
-            color: Color(0x000000),
+            fontSize: 28,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
           ),
           display2: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.normal,
-            color: Color(0x000000),
+            fontSize: 32,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
           ),
           display3: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.normal,
-            color: Color(0x000000),
+            fontSize: 36,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
+          ),
+          display4: TextStyle(
+            fontSize: 38,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
           ),
 
         ),
@@ -69,51 +123,143 @@ class MyApp extends StatelessWidget {
 
         inputDecorationTheme: InputDecorationTheme(
           focusColor: Color(0x6CC4DC),
+          labelStyle: TextStyle(
+            fontSize: 18,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(48.0),
+            borderSide: BorderSide(),
+          ),
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: 20, vertical: 14
+          ),
         ),
-
+       
       ),
       
       
-      initialRoute: AppRoutes.TUTORIAL,
+      initialRoute: AppRoutes.BASE,
       onGenerateRoute: AppRoutes.generate,
+
+      // home: ThemesTest(),
 
       debugShowCheckedModeBanner: false,
 
     );
+    
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
 
+// test themes
+class ThemesTest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('widget.title'),
-      ),
-      body: Center(
 
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      appBar: AppBar(
+        title: Text('Test Theme'),
+        centerTitle: true,
+      ),
+
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+
+          // title
+          Text(
+            'title text',
+            style: Theme.of(context).textTheme.title.copyWith(
+              color: AppColors.primary
             ),
-            Text(
-              '0',
-              style: Theme.of(context).textTheme.display1,
+          ),
+
+          SizedBox(height: 40,),
+
+          // subtitle
+          Text(
+            'subtitle text',
+            style: Theme.of(context).textTheme.subtitle.copyWith(
+              color: AppColors.secondary
             ),
-          ],
-        ),
+          ),
+
+          SizedBox(height: 40,),
+
+
+          // subhead
+          Text(
+            'subhead text',
+            style: Theme.of(context).textTheme.subhead.copyWith(
+              color: AppColors.black
+            ),
+          ),
+
+          SizedBox(height: 40,),
+
+          // body 1
+          Text(
+            'body 1 text',
+            style: Theme.of(context).textTheme.body1,
+          ),
+
+          SizedBox(height: 40,),          
+
+          // body 2
+          Text(
+            'body 2 text',
+            style: Theme.of(context).textTheme.body2,
+          ),
+
+          SizedBox(height: 40,),
+
+          // display 1
+          Text(
+            'display 1 text',
+            style: Theme.of(context).textTheme.display1,
+          ),
+
+          SizedBox(height: 40,),          
+
+          // display 2
+          Text(
+            'display 2 text',
+            style: Theme.of(context).textTheme.display2,
+          ),
+
+          SizedBox(height: 40,),          
+
+          // display 3
+          Text(
+            'display 3 text',
+            style: Theme.of(context).textTheme.display3,
+          ),
+
+          SizedBox(height: 40,),          
+
+          // display 4
+          Text(
+            'display 4 text',
+            style: Theme.of(context).textTheme.display4,
+          ),
+
+          
+          Text(
+            'display 4 text',
+            style: TextStyle(
+              color: Color(0x3F92B7),
+            ),
+          ),
+           
+
+        ],
       ),
       
     );
   }
 }
+
+
+
