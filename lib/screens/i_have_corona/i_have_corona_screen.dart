3551647eda8b72b252cea12517eab31ea8b9f294
Efen Stakes <efenstakes101@gmail.com>
2020-04-16 00:03:25 +0300

    add a screen to report a corona case

--- lib/screens/i_have_corona/i_have_corona_screen.dart
+++ lib/screens/i_have_corona/i_have_corona_screen.dart
@@ -85,12 +85,17 @@ class _IHaveCoronaState extends State<IHaveCoronaScreen> with TickerProviderStat
   Widget _buildBody({ BuildContext context }) {
 
     if( this._showNotYetTestedWidget ) {
-      return _buildNotTestedWidget(context: context);
-      // return YetToTest(goHome: this._goHome, goCheckSymptoms: this._goCheckSymptoms,);
+      return YetToTest(
+        goHome: this._goHome, 
+        goCheckSymptoms: this._goCheckSymptoms,
+      );
     }
 
     return _hasReported 
-               ? _buildReportSuccessBody(context: context)
+               ? YetToTest(
+                   goHome: this._goHome, 
+                   goCheckSymptoms: this._goCheckSymptoms,
+                 )
                : _buildReportBody(context: context);
   }
 
@@ -224,100 +229,6 @@ class _IHaveCoronaState extends State<IHaveCoronaScreen> with TickerProviderStat
       ),
     );
   }// Widget _buildReportBody({ BuildContext context }) { .. }
-
-
-  // show after submitting the case
-  Widget _buildReportSuccessBody({ BuildContext context }) {   
-    Size screenSize = MediaQuery.of(context).size;
-
-    return Container(
-      width: screenSize.width,
-      padding: EdgeInsets.only(left: 24, right: 24),
-      color: AppColors.secondary,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-
-          Center(
-            child: Text(
-              'Thank you for reporting',
-              style: Theme.of(context).textTheme.title,
-            ),
-          ),
-
-          SizedBox(height: 20,),
-
-          Text(
-            'Your case has been reported successfully. We wish you a quick recovery',
-            style: Theme.of(context).textTheme.body1,
-            textAlign: TextAlign.center,
-          ),
-
-          SizedBox(height: 60,),
-          
-          Center(
-            child: FloatingActionButton.extended(
-              label: Text('Go Back'),
-              onPressed: ()=> this._goHome(context: context),
-            ),
-          ),
-         
- 
-        ],
-      ),
-    );
-  }// Widget _buildReportSuccessBody({ BuildContext context }) { .. }
-
-
-  // widget to show when user has not been tested
-  Widget _buildNotTestedWidget({ BuildContext context }) {
-    Size screenSize = MediaQuery.of(context).size;
-
-    return Container(
-      width: screenSize.width,
-      color: AppColors.secondary,
-      padding: EdgeInsets.only(left: 24, right: 24),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-
-          Center(
-            child: Text(
-              'Get tested first',
-              style: Theme.of(context).textTheme.title,
-            ),
-          ),
-
-          SizedBox(height: 20,),
-
-          Text(
-            'Check whether your symptoms match those of corona and ascertain whether you need to visit a doctor',
-            style: Theme.of(context).textTheme.body1,
-            textAlign: TextAlign.center,
-          ),
-
-          SizedBox(height: 60,),
-          
-          Center(
-            child: FloatingActionButton.extended(
-              label: Text('Check Symptoms'),
-              onPressed: ()=> this._goCheckSymptoms(context: context),
-            ),
-          ),
-
-          SizedBox(height: 60,),
-         
-          Center(
-            child: FlatButton(
-              child: Text('Go Back'),
-              onPressed: ()=> this._goHome(context: context),
-            ),
-          ),
-
-        ],
-      ),
-    ); 
-  }// Widget _buildNotTestedWidget({ BuildContext context }) { .. }
 
 
   Widget _buildCoronaImage({ BuildContext context }) {
