534f5f541d6ae1715d62e563c26d5f9c55f37441
Efen Stakes <efenstakes101@gmail.com>
2020-04-17 20:22:31 +0300

    add an account service to add users into shared preferences when they first use the app

---
+++ lib/services/local_account_service.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:kiloko/config/app_utils.dart';
+import 'package:kiloko/services/utils_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class LocalAccountService {
+
+  LocalAccountService();
+
+
+
+  void newAccount() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+    String accountCode = UtilsService.generateCode(length: 7);
+
+    String existingCode = await sharedPreferences.getString(SharedPreferenceVars.accountID);
+
+    if( existingCode.isNotEmpty ) {
+      await sharedPreferences.setString(
+        SharedPreferenceVars.accountID, accountCode
+      );
+    }
+  }// void newAccount() async { .. }
+
+}
