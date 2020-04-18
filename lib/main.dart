4356b20bf74f471c1fe9b18afcfae842110ddeb8
Efen Stakes <efenstakes101@gmail.com>
2020-04-18 16:14:13 +0300

    add providers to main app scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,153 +1,174 @@
 import 'package:flutter/material.dart';
 import 'package:kiloko/config/app_routes.dart';
 import 'package:kiloko/config/app_utils.dart';
+import 'package:kiloko/providers/local_condition_provider.dart';
+import 'package:kiloko/providers/local_medication_provider.dart';
+import 'package:provider/provider.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Kiloko',
+    return MultiProvider(
 
-      theme: ThemeData(
+      providers: [
+
+        // medication
+        ChangeNotifierProvider<LocalMedicationProvider>(
+          create: (BuildContext contxt)=> LocalMedicationProvider(),
+        ),
         
-        // primarySwatch: AppColors.primary,
-        primaryColor: AppColors.primary,
-        buttonColor: AppColors.primary,
-        errorColor: AppColors.error,
-        
-
-        buttonTheme: ButtonThemeData(
-          buttonColor: AppColors.primary,
-          splashColor: AppColors.tertiary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(40),
-          ),
+        // condition
+        ChangeNotifierProvider<LocalConditionProvider>(
+          create: (BuildContext contxt)=> LocalConditionProvider(),
         ),
 
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: AppColors.primary,
-          elevation: 6,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(40),
-          ),
-        ),
+      ],
 
-        primaryIconTheme: IconThemeData(
-          size: 28,
-          color: AppColors.white
-        ),
+      child: MaterialApp(
+        title: 'Kiloko',
 
-        accentIconTheme: IconThemeData(
-          size: 32,
-          color: AppColors.white
-        ),
-
-        appBarTheme: AppBarTheme(
-          color: AppColors.secondary,
-          elevation: 10,
-          textTheme: TextTheme(
-            title: Theme.of(context).textTheme.title
-          ),
-          iconTheme: IconThemeData(
-            size: 28,
-            color: Colors.black
-          ),
-          actionsIconTheme: IconThemeData(
-            size: 28,
-            color: Colors.black
-          ),
-        ),
-
-        textTheme: TextTheme(
+        theme: ThemeData(
           
-          title: TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.w600,
-            // color: Color(0xFFFF9000),
-            color: Colors.black,
-          ),
-          subtitle: TextStyle(
-            fontSize: 26,
-            fontWeight: FontWeight.w600,
-            // color: Color(0x0000).withOpacity(0.8),
-            color: Colors.black,
-          ),
-          subhead: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
-            // color: Color(0x000000).withOpacity(0.8),
-            color: Colors.black,
+          // primarySwatch: AppColors.primary,
+          primaryColor: AppColors.primary,
+          buttonColor: AppColors.primary,
+          errorColor: AppColors.error,
+          
+
+          buttonTheme: ButtonThemeData(
+            buttonColor: AppColors.primary,
+            splashColor: AppColors.tertiary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40),
+            ),
           ),
 
-          body1: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.normal,
-            // color: Color(0x000000),
-            color: Colors.black,
-          ),
-          body2: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.normal,
-            // color: Color(0x000000),
-            color: Colors.black,
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: AppColors.primary,
+            elevation: 6,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40),
+            ),
           ),
 
-          display1: TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.w600,
-            color: Colors.black,
-          ),
-          display2: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.w600,
-            color: Colors.black,
-          ),
-          display3: TextStyle(
-            fontSize: 36,
-            fontWeight: FontWeight.w600,
-            color: Colors.black,
-          ),
-          display4: TextStyle(
-            fontSize: 38,
-            fontWeight: FontWeight.w600,
-            color: Colors.black,
+          primaryIconTheme: IconThemeData(
+            size: 28,
+            color: AppColors.white
           ),
 
+          accentIconTheme: IconThemeData(
+            size: 32,
+            color: AppColors.white
+          ),
+
+          appBarTheme: AppBarTheme(
+            color: AppColors.secondary,
+            elevation: 10,
+            textTheme: TextTheme(
+              title: Theme.of(context).textTheme.title
+            ),
+            iconTheme: IconThemeData(
+              size: 28,
+              color: Colors.black
+            ),
+            actionsIconTheme: IconThemeData(
+              size: 28,
+              color: Colors.black
+            ),
+          ),
+
+          textTheme: TextTheme(
+            
+            title: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.w600,
+              // color: Color(0xFFFF9000),
+              color: Colors.black,
+            ),
+            subtitle: TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.w600,
+              // color: Color(0x0000).withOpacity(0.8),
+              color: Colors.black,
+            ),
+            subhead: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+              // color: Color(0x000000).withOpacity(0.8),
+              color: Colors.black,
+            ),
+
+            body1: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.normal,
+              // color: Color(0x000000),
+              color: Colors.black,
+            ),
+            body2: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.normal,
+              // color: Color(0x000000),
+              color: Colors.black,
+            ),
+
+            display1: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
+            display2: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
+            display3: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
+            display4: TextStyle(
+              fontSize: 38,
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
+
+          ),
+
+          iconTheme: IconThemeData(
+            color: Color(0x000000),
+          ),
+
+          inputDecorationTheme: InputDecorationTheme(
+            focusColor: Color(0x6CC4DC),
+            labelStyle: TextStyle(
+              fontSize: 18,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(48.0),
+              borderSide: BorderSide(),
+            ),
+            contentPadding: EdgeInsets.symmetric(
+              horizontal: 20, vertical: 14
+            ),
+          ),
+        
         ),
+        
+        
+        initialRoute: AppRoutes.BASE,
+        onGenerateRoute: AppRoutes.generate,
 
-        iconTheme: IconThemeData(
-          color: Color(0x000000),
-        ),
+        // home: ThemesTest(),
 
-        inputDecorationTheme: InputDecorationTheme(
-          focusColor: Color(0x6CC4DC),
-          labelStyle: TextStyle(
-            fontSize: 18,
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(48.0),
-            borderSide: BorderSide(),
-          ),
-          contentPadding: EdgeInsets.symmetric(
-            horizontal: 20, vertical: 14
-          ),
-        ),
-       
+        debugShowCheckedModeBanner: false,
+
       ),
-      
-      
-      initialRoute: AppRoutes.BASE,
-      onGenerateRoute: AppRoutes.generate,
-
-      // home: ThemesTest(),
-
-      debugShowCheckedModeBanner: false,
-
     );
     
-  }
+  }// Widget build(BuildContext context) { .. }
 }
 
 
