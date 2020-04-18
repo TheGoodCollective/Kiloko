55f8cde41ec535902f91322762a95e3574720d85
Efen Stakes <efenstakes101@gmail.com>
2020-04-18 17:00:35 +0300

    add a local provider for unauthenticated users to only provision a kiloko id

---
+++ lib/providers/local_account_provider.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:kiloko/config/app_utils.dart';
+import 'package:kiloko/models/account.dart';
+import 'package:kiloko/services/local_account_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+
+class LocalAccountProvider with ChangeNotifier {
+  Account _account;
+  LocalAccountService _localAccountService;
+
+  LocalAccountProvider() {
+    _localAccountService = LocalAccountService();
+    this._getAccount();
+  }// LocalAccountProvider() { .. }
+
+
+  // get user account
+  Account get account=> this._account;
+
+
+  Future<Account> _getAccount() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+    try {
+      String kilokoID = sharedPreferences.getString(SharedPreferenceVars.kilokoID);
+      this._account = Account(kilokoID: kilokoID);
+    } catch (e) {
+    }
+
+
+    notifyListeners();
+    return null;
+  }// Future<Account> _getAccount() async { .. }
+
+}
