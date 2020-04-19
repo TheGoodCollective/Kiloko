d0ace228ba92dbdf076be7a105902409be6595c8
Efen Stakes <efenstakes101@gmail.com>
2020-04-19 15:25:04 +0300

    create a standalone widget to show when user has no conditions

---
+++ lib/screens/my_conditions/no_conditions_widget.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:kiloko/config/app_utils.dart';
+
+
+
+class NoConditionsWidget extends StatelessWidget {
+  final Function goViewWhy;
+
+  NoConditionsWidget({ this.goViewWhy });
+
+
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      padding: EdgeInsets.only(left: 16, right: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+
+          SizedBox(height: screenSize.height/8),
+          
+          Center(
+            child: CircleAvatar(
+              radius: screenSize.width/6,
+              backgroundImage: AssetImage(AppImages.corona),
+            ),
+          ),
+
+          SizedBox(height: 56,),
+
+          Center(
+            child: Text(
+              'No Conditions',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+
+          SizedBox(height: 32,),
+          
+          Text(
+            'Incase you have any conditions, click below button to add it. Your conditions will appear here',
+            style: Theme.of(context).textTheme.body2,
+            textAlign: TextAlign.center,
+          ),
+ 
+          SizedBox(height: 80,),
+          
+          GestureDetector(
+
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                
+                Icon(
+                  Icons.info,
+                  color: AppColors.primary,
+                ),
+
+                SizedBox(width: 8,),
+                
+                Text(
+                  'Why should I add',
+                  style: Theme.of(context).textTheme.body1.copyWith(
+                    color: AppColors.primary,
+                  )
+                ),
+
+              ],
+            ),
+
+            onTap: this.goViewWhy,
+          ),
+
+        ],
+      ),
+    );
+  }// Widget build(BuildContext context) { .. }
+}
